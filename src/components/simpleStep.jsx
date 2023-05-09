64a4cf2d0945ee89d8f82bc8a60dc74331fdde61
zhouzihao <1042181618@qq.com>
2023-05-09 22:36:39 +0800

    dev-添加获胜条件

--- src/components/simpleStep.jsx
+++ src/components/simpleStep.jsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Step, StepButton, Stepper, Typography } from "@mui/material";
+import { FormControlLabel, Grid, Paper, Radio, RadioGroup, Step, StepButton, Stepper, Typography } from "@mui/material";
 import { useState } from "react";
 import ExpandCard from "./expendCard";
 import { useTranslation } from "react-i18next";
@@ -8,12 +8,20 @@ import './simpleStep.css'
 function SimpleStep() {
     const { t } = useTranslation();
     const [activeStep, setActiveStep] = useState(0);
+    const [chose, setChose] = useState(null);
     const stepList = [
         { step: 0, label: t("游戏准备") },
         { step: 1, label: t("进行阶段1") },
         { step: 2, label: t("进行阶段2") },
         { step: 3, label: t("游戏终止") }
     ];
+
+    const handleChange = (event) => {
+        console.log(chose)
+        console.log('>>>>>>>>>>>>>>')
+        console.log(event.target.value);
+        setChose(event.target.value);
+    };
 
     const handleStep = (step) => () => {
         setActiveStep(step);
@@ -198,6 +206,75 @@ function SimpleStep() {
                                     </Grid>
                                     <Grid item xs={8}>
                                         <img src={t("case_art_pic")} />
+                                    </Grid>
+                                </Grid>
+                            </>
+                        )}
+                        {/* 游戏结束相关的内容 */}
+                        {activeStep === 3 && (
+                            <>
+                                <Grid
+                                    container
+                                    spacing={2}
+                                    justifyContent="center">
+                                    <Grid item xs={8}>
+                                        <Typography variant="h6" >
+                                            {t("那么如何判断游戏结束和哪方获胜呢？每次回合结束的时候你们要看泳道内的5个区域是否都被摆满了卡。如果泳道内的卡满了。那么就就终止游戏，开始判断输赢了。")}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={8}>
+                                        <Typography variant="h6" >
+                                            {t("正如上面所述的一样。你要计算出泳道内值和算子构成的值。然后每个玩家再用这个值和自己断言区内的每个断言卡进行组合，断言为真胜利点数加上VP,断言为假胜利点数减去DP。然后比较两个那个胜利点数大，大的一方获得胜利。")}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={8}>
+                                        <Typography variant="h6" >
+                                            {t("听起来很简单是不是？在游戏中你要预测泳道可能出现的值，并且规划好设置断言的策略，如果设置的太多可能反而让自己失败。")}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={10}>
+                                        <ExpandCard m={5} title={t("测试一下!")} open={true} >
+                                            <Grid
+                                                container
+                                                spacing={2}
+                                                justifyContent="center">
+                                                <Grid item xs={2}>
+                                                    <Typography variant="h6">
+                                                        {t("谁赢了？")}
+                                                    </Typography>
+                                                </Grid>
+                                                <Grid item xs={2}>
+                                                    <RadioGroup
+                                                        row
+                                                        value={chose}
+                                                        onChange={handleChange}>
+                                                        <FormControlLabel value={0} control={<Radio />} label={t("上方")} />
+                                                        <FormControlLabel value={1} control={<Radio />} label={t("下方")} />
+                                                    </RadioGroup>
+                                                </Grid>
+                                                {
+                                                    chose == 1 && (
+                                                        <Grid item xs={10}>
+                                                            <Typography variant="h6" fontStyle={{ color: 'green' }}>
+                                                                {t("恭喜你答对了！")}
+                                                            </Typography>
+                                                        </Grid>
+                                                    )
+                                                }
+                                                {
+                                                    chose == 0 && (
+                                                        <Grid item xs={10}>
+                                                            <Typography variant="h6" fontStyle={{ color: 'red' }} >
+                                                                {t("很遗憾.下方获胜了。泳道的值为1+1+1=3。是奇数。下方偶数断言卡一张。总胜利点数-1而上面有两张胜利点数-2。(-1>-2)")}
+                                                            </Typography>
+                                                        </Grid>
+                                                    )
+                                                }
+                                                <Grid item xs={10}>
+                                                    <img src={t("case_art_pic")} />
+                                                </Grid>
+                                            </Grid>
+                                        </ExpandCard>
                                     </Grid>
                                 </Grid>
                             </>
