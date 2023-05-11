38292e688342adb906516a8165a16a536a09a5c3
zhouzihao <1042181618@qq.com>
2023-05-11 17:00:52 +0800

    dev-数据从其他地方获取

--- src/routes/cardDb.jsx
+++ src/routes/cardDb.jsx
@@ -1,101 +1,62 @@
 import { Grid, ImageList, Paper } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import CardBox from "../components/card/cardBox";
+import { useState } from "react";
+import { Category } from "@mui/icons-material";
 
 function CardDb() {
-    const { t } = useTranslation()
+    const { t, i18n } = useTranslation()
 
-    const cardList = [
-        {
+    const [cardList, setCardList] = useState([]);
+
+    fetch(`https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/${t('db')}`)
+        .then(response => response.json())
+        .then(data => {
+            setCardList(data);
+        })
+        .catch(error => console.error(error));
+
+    // const cardList = [
+    //     {
+    //         size: 300,
+    //         _id: "000003",
+    //         name: t('sample3.name'),
+    //         lang: t('sample3.lang'),
+    //         type: 'assertion',
+    //         desc: t('sample3.desc'),
+    //         formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000003.svg",
+    //         pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000003.png",
+    //         lazy: true
+    //     },
+    //     {
+    //         "_id": "000001",
+    //         "name": "Junior Number One",
+    //         "type": "value-assertion",
+    //         "desc": "When set this card in [Value Area], you can take a card with [Natural] Category from desk to your hand.",
+    //         "category": [
+    //             "Natural",
+    //             "Assertion",
+    //             "Odd"
+    //         ],
+    //         "author": "robzhou",
+    //         "formulaPic": "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
+    //         "pic": "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
+    //         "vp": 1,
+    //         "dp": 1
+    //     }
+    // ]
+
+    function packageData(item) {
+        return {
+            ...item,
+            lang: i18n.language,
             size: 300,
-            _id: "000003",
-            name: t('sample3.name'),
-            lang: t('sample3.lang'),
-            type: 'assertion',
-            desc: t('sample3.desc'),
-            formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000003.svg",
-            pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000003.png",
-            lazy: true
-        },
-        {
-            size: 300,
-            _id: "000002",
-            name: t('sample2.name'),
-            lang: t('sample2.lang'),
-            type: 'operator',
-            desc: t('sample2.desc'),
-            formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
-            pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
-            lazy: true
-        },
-        {
-            size: 300,
-            _id: "000001",
-            name: t('sample.name'),
-            lang: t('sample.lang'),
-            type: 'value',
-            desc: t('sample.desc'),
-            // formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
-            // pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
-            lazy: true
-        },
-        {
-            size: 300,
-            _id: "000002",
-            name: t('sample2.name'),
-            lang: t('sample2.lang'),
-            type: 'operator',
-            desc: t('sample2.desc'),
-            formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
-            pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
-            lazy: true
-        },
-        {
-            size: 300,
-            _id: "000002",
-            name: t('sample2.name'),
-            lang: t('sample2.lang'),
-            type: 'operator',
-            desc: t('sample2.desc'),
-            formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
-            pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
-            lazy: true
-        },
-        {
-            size: 300,
-            _id: "000002",
-            name: t('sample2.name'),
-            lang: t('sample2.lang'),
-            type: 'operator',
-            desc: t('sample2.desc'),
-            formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
-            pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
-            lazy: true
-        },
-        {
-            size: 300,
-            _id: "000002",
-            name: t('sample2.name'),
-            lang: t('sample2.lang'),
-            type: 'operator',
-            desc: t('sample2.desc'),
-            formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
-            pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
-            lazy: true
-        },
-        {
-            size: 300,
-            _id: "000002",
-            name: t('sample2.name'),
-            lang: t('sample2.lang'),
-            type: 'operator',
-            desc: t('sample2.desc'),
-            formulaPic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/000002.svg",
-            pic: "https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/000002.png",
-            lazy: true
+            lazy: true,
+            formulaPic: item.formulaPic ? item.formulaPic : `https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/svg/${item._id}.svg`,
+            pic: item.pic ? item.pic : `https://raw.githubusercontent.com/TCG-Math-Magic-Card/db/main/pic/${item._id}.png`,
+            desc: `${t("category")}:${item.category.join('/')}\r\n${item.desc}`
         }
-    ]
-
+    }
 
     return (
         <>
@@ -113,13 +74,13 @@ function CardDb() {
                     >
                         {cardList.map(item => {
                             return (
-                                <CardBox {...item} />
+                                <CardBox {...packageData(item)} />
                             );
                         })}
                     </ImageList>
                 </Grid>
                 <Grid item xs={1}>
-                    <Paper elevation={12} style={{ height: '100%' }}>这里是筛选条件</Paper>
+                    <Paper elevation={12} style={{ height: '100%' }}>Coming soon……</Paper>
                 </Grid>
             </Grid>
         </>
